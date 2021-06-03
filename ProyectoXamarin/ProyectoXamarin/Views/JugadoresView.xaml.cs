8e24d4c5256bd3032905f79978320b833269dfda
George Francis Shinner Rodriguez <georgeshinner@gmail.com>
2021-06-03 20:08:42 +0200

    sdfg

--- ProyectoXamarin/ProyectoXamarin/Views/JugadoresView.xaml.cs
+++ ProyectoXamarin/ProyectoXamarin/Views/JugadoresView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ProyectoXamarin.Models;
+using ProyectoXamarin.Services;
+using ProyectoXamarin.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +15,45 @@ namespace ProyectoXamarin.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class JugadoresView : ContentPage
     {
+        private ServiceEquipos Service;
+
         public JugadoresView()
         {
             InitializeComponent();
-            //this.collectionviewjugadores.selec += ListViewJugadores_ItemSelected;
         }
 
-        //private void ListViewJugadores_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        //{
-        //    if (e.SelectedItem == null) return;
+        public Command DetallesJugador
+        {
+            get
+            {
+                return new Command(async (jug) =>
+                {
+                    Jugador jugador = jug as Jugador;
+                    JugadorViewModel viewmodel = App.ServiceLocator.JugadorViewModel;
+                    viewmodel.Jugador = jugador;
+                    DetailsJugadorView view = new DetailsJugadorView();
+                    view.BindingContext = viewmodel;
+                    await Application.Current.MainPage.Navigation.PushModalAsync(view);
+                });
+            }
+        }
 
-        //    if (sender is ListView lv) lv.SelectedItem = null;
-        //}
+        public Command DetallesEquipo
+        {
+            get
+            {
+                return new Command(async (jug) =>
+                {
+                    this.Service = new ServiceEquipos();
+                    Jugador jugador = jug as Jugador;
+                    EquipoViewModel viewmodel = App.ServiceLocator.EquipoViewModel;
+                    Equipo equipo = await this.Service.BuscarEquipoAsync(jugador.IdEquipo);
+                    viewmodel.Equipo = equipo;
+                    DetailsEquipoView view = new DetailsEquipoView();
+                    view.BindingContext = viewmodel;
+                    await Application.Current.MainPage.Navigation.PushModalAsync(view);
+                });
+            }
+        }
     }
 }
